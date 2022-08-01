12a027cafc1b57ec1531b55ec2634880cb527133
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-01 16:44:19 -0500

    resolve syntax issue with outputs

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "id" {
 
 output "standard_srv" {
   description = "mongo uri endpoint"
-  value       = var.create_dedicated_cluster ? mongodbatlas_cluster.dedicated.*.connection_strings.standard_srv : mongodbatlas_cluster.this.*.connection_strings.standard_srv
+  value       = var.create_dedicated_cluster ? mongodbatlas_cluster.dedicated.0.connection_strings[0].standard_srv : mongodbatlas_cluster.this.0.connection_strings[0].standard_srv
 }
 # Example return string: standard_srv = "mongodb+srv://cluster-atlas.ygo1m.mongodb.net"
 
@@ -17,7 +17,7 @@ output "password" {
 
 output "cluster_connection_string" {
   description = "trimmed down mongo uri endpoint"
-  value       = var.create_dedicated_cluster ? split("//", mongodbatlas_cluster.dedicated.*.connection_strings.standard_srv)[1] : split("//", mongodbatlas_cluster.this.*.connection_strings.standard_srv)[1]
+  value       = var.create_dedicated_cluster ? split("//", mongodbatlas_cluster.dedicated.0.connection_strings[0].standard_srv)[1] : split("//", mongodbatlas_cluster.this.0.connection_strings[0].standard_srv)[1]
 }
 
 output "project_id" {
