3d94551b487de29fc753195827cb3af74b0c6f0d
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-02 15:57:56 -0500

    updating cluster connection string

--- outputs.tf
+++ outputs.tf
@@ -17,7 +17,7 @@ output "password" {
 
 output "cluster_connection_string" {
   description = "trimmed down mongo uri endpoint"
-  value       = trimprefix("mongodb+srv://", mongodbatlas_cluster.this[*].connection_strings[0].standard_srv)
+  value       = var.enable_free_cluster ? split("//", mongodbatlas_cluster.this[1].connection_strings[0].standard_srv)[1] : split("//", mongodbatlas_cluster.this[0].connection_strings[0].standard_srv)[1]
 }
 
 output "project_id" {
