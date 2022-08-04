9583b8d96e131fc87642a610ac6b8ad5eb22483c
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-04 12:35:05 -0500

    adding cluster restore resource for testing

--- cluster-restore.tf
+++ cluster-restore.tf
@@ -1,11 +1,11 @@
 resource "mongodbatlas_cloud_backup_snapshot_restore_job" "restore" {
-  count        = var.enable_cluster_restore && var.restore_project_id != null && var.restore_cluster_name != null && var.restore_snapshot_id != null ? 1 : 0
-  project_id   = var.restore_project_id
-  cluster_name = var.restore_cluster_name
+  count        = var.enable_cluster_restore && var.restore_snapshot_id != null ? 1 : 0
+  project_id   = mongodbatlas_cloud_backup_snapshot.this[0].project_id
+  cluster_name = mongodbatlas_cloud_backup_snapshot.this[0].cluster_name
   snapshot_id  = var.restore_snapshot_id
   delivery_type {
     automated           = true
-    target_cluster_name = var.restore_cluster_name
-    target_project_id   = var.restore_project_id
+    target_cluster_name = mongodbatlas_cloud_backup_snapshot.this[0].cluster_name
+    target_project_id   = mongodbatlas_cloud_backup_snapshot.this[0].project_id
   }
 }
