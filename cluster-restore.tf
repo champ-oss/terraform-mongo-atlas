d6dac4819ce3bab304ebb07bb25f0f7a4e73d195
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-08-04 14:40:36 -0500

    adding restore project id

--- cluster-restore.tf
+++ cluster-restore.tf
@@ -1,17 +1,11 @@
 resource "mongodbatlas_cloud_backup_snapshot_restore_job" "restore" {
-  count        = var.enable_cluster_restore && var.restore_snapshot_id != null ? 1 : 0
+  count        = var.enable_cluster_restore && var.restore_snapshot_id != null && var.restore_project_id != null ? 1 : 0
   project_id   = mongodbatlas_cloud_backup_snapshot.this[0].project_id
   cluster_name = mongodbatlas_cloud_backup_snapshot.this[0].cluster_name
   snapshot_id  = var.restore_snapshot_id
   delivery_type_config {
     automated           = true
-    target_cluster_name = data.mongodbatlas_cloud_backup_snapshot_restore_job.this.cluster_name
-    target_project_id   = data.mongodbatlas_cloud_backup_snapshot_restore_job.this.project_id
+    target_cluster_name = var.cluster_name
+    target_project_id   = var.restore_project_id
   }
-}
-
-data "mongodbatlas_cloud_backup_snapshot_restore_job" "this" {
-  project_id   = mongodbatlas_cloud_backup_snapshot_restore_job.this[0].project_id
-  cluster_name = mongodbatlas_cloud_backup_snapshot_restore_job.this[0].cluster_name
-  job_id       = mongodbatlas_cloud_backup_snapshot_restore_job.this[0].id
 }
