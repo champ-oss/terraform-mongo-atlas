c0ae116a5afb20ef990ec45b992f37ccd61fd16a
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-08-04 14:29:01 -0500

    updating resource

--- cluster-restore.tf
+++ cluster-restore.tf
@@ -5,7 +5,13 @@ resource "mongodbatlas_cloud_backup_snapshot_restore_job" "restore" {
   snapshot_id  = var.restore_snapshot_id
   delivery_type_config {
     automated           = true
-    target_cluster_name = mongodbatlas_cloud_backup_snapshot.this[0].cluster_name
-    target_project_id   = mongodbatlas_cloud_backup_snapshot.this[0].project_id
+    target_cluster_name = data.mongodbatlas_cloud_backup_snapshot_restore_job.this.cluster_name
+    target_project_id   = data.mongodbatlas_cloud_backup_snapshot_restore_job.this.project_id
   }
+}
+
+data "mongodbatlas_cloud_backup_snapshot_restore_job" "this" {
+  project_id     = mongodbatlas_cloud_backup_snapshot_restore_job.this.project_id
+  cluster_name = mongodbatlas_cloud_backup_snapshot_restore_job.this.cluster_name
+  job_id       = mongodbatlas_cloud_backup_snapshot_restore_job.this.id
 }
