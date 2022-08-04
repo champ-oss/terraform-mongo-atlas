4e798d78885fbf693a51b153217a8e06c998de83
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-08-04 10:09:39 -0500

    updating few desc and comments

--- cluster.tf
+++ cluster.tf
@@ -3,6 +3,7 @@ resource "mongodbatlas_cluster" "this" {
   name       = var.cluster_name
 
   # Provider Settings "block"
+  # setting backing provider name to null as it's not available for provider_instance_size_name >= M10
   provider_name               = var.provider_name == "" ? "TENANT" : var.provider_name
   backing_provider_name       = var.provider_name == "" ? "AWS" : null
   provider_region_name        = var.mongo_region
@@ -10,6 +11,7 @@ resource "mongodbatlas_cluster" "this" {
   mongo_db_major_version      = var.mongo_db_major_version
   cluster_type                = var.cluster_type
 
+  # use replication specs for provider_instance_size_name >= M10
   dynamic "replication_specs" {
     for_each = var.replication_specs != null ? var.replication_specs : []
     content {
@@ -24,6 +26,7 @@ resource "mongodbatlas_cluster" "this" {
     }
   }
 
+  # configurable for provider_instance_size_name >= M10
   cloud_backup                 = var.cloud_backup
   auto_scaling_disk_gb_enabled = var.auto_scaling_disk_gb_enabled
   disk_size_gb                 = var.disk_size_gb
