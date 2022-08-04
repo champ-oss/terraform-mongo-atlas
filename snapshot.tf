4c2b5ba4096dc098aa31a33173eec05a4a82d55f
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-08-03 20:27:55 -0500

    updating snapshot resource

--- snapshot.tf
+++ snapshot.tf
@@ -1,20 +1,21 @@
-resource "mongodbatlas_cloud_provider_snapshot" "this" {
+resource "mongodbatlas_cloud_backup_snapshot" "this" {
   count             = var.enable_mongo_snapshot ? 1 : 0
   project_id        = mongodbatlas_cluster.this.project_id
   cluster_name      = mongodbatlas_cluster.this.name
   description       = "atlas cluster snapshot"
+  timeout           = "10m"
   retention_in_days = 1
 }
 
-resource "mongodbatlas_cloud_provider_snapshot_restore_job" "this" {
+resource "mongodbatlas_cloud_backup_snapshot_restore_job" "this" {
   count        = var.enable_mongo_snapshot && var.target_cluster_name != null && var.target_project_id != null ? 1 : 0
-  project_id   = mongodbatlas_cloud_provider_snapshot.this[0].project_id
-  cluster_name = mongodbatlas_cloud_provider_snapshot.this[0].cluster_name
-  snapshot_id  = mongodbatlas_cloud_provider_snapshot.this[0].snapshot_id
+  project_id   = mongodbatlas_cloud_backup_snapshot.this[0].project_id
+  cluster_name = mongodbatlas_cloud_backup_snapshot.this[0].cluster_name
+
   delivery_type_config {
     automated           = true
     target_cluster_name = var.target_cluster_name
     target_project_id   = var.target_project_id
   }
-  depends_on = [mongodbatlas_cloud_provider_snapshot.this]
+  depends_on = [mongodbatlas_cloud_backup_snapshot.this]
 }
