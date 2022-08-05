88b3914640369dab2f6972c64c2d900aea70f5bc
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-05 15:41:43 -0500

    resolve syntax issue

--- util-snapshot-target-restore.tf
+++ util-snapshot-target-restore.tf
@@ -1,8 +1,3 @@
-data "mongodbatlas_clusters" "snapshot_target_restore" {
-  count      = var.enable_snapshot_target_restore && var.target_project_id != null ? 1 : 0
-  project_id = var.target_project_id
-}
-
 resource "mongodbatlas_cloud_backup_snapshot" "snapshot_target_restore" {
   count             = var.enable_snapshot_target_restore ? 1 : 0
   project_id        = mongodbatlas_cluster.this.project_id
@@ -12,15 +7,15 @@ resource "mongodbatlas_cloud_backup_snapshot" "snapshot_target_restore" {
 }
 
 resource "mongodbatlas_cloud_backup_snapshot_restore_job" "snapshot_target_restore" {
-  count        = var.enable_snapshot_target_restore != null ? 1 : 0
+  count        = var.enable_snapshot_target_restore && var.target_cluster_name != null && var.target_project_id != null ? 1 : 0
   project_id   = mongodbatlas_cloud_backup_snapshot.snapshot_target_restore[0].project_id
   cluster_name = mongodbatlas_cloud_backup_snapshot.snapshot_target_restore[0].cluster_name
   snapshot_id  = mongodbatlas_cloud_backup_snapshot.snapshot_target_restore[0].snapshot_id
 
   delivery_type_config {
     automated           = true
-    target_cluster_name = data.mongodbatlas_clusters.snapshot_target_restore[0].results[0].name
-    target_project_id   = data.mongodbatlas_clusters.snapshot_target_restore[0].project_id
+    target_cluster_name = var.target_cluster_name
+    target_project_id   = var.target_project_id
   }
   depends_on = [mongodbatlas_cloud_backup_snapshot.snapshot_target_restore]
 }
--- variables.tf
+++ variables.tf
@@ -130,6 +130,12 @@ variable "target_project_id" {
   default     = null
 }
 
+variable "target_cluster_name" {
+  description = "https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/resources/cloud_provider_snapshot_restore_job#cluster_name"
+  type        = string
+  default     = null
+}
+
 variable "enable_cluster_restore" {
   description = "https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/data-sources/cloud_backup_snapshot_restore_job"
   type        = bool
