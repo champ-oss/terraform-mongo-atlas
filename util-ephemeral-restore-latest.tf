98a1faed8647af3348a49a6dab3df951e90f5e12
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-05 12:40:00 -0500

    updating snapshot id field

--- util-ephemeral-restore-latest.tf
+++ util-ephemeral-restore-latest.tf
@@ -10,7 +10,7 @@ resource "mongodbatlas_cloud_backup_snapshot_restore_job" "ephemeral_restore_lat
   count        = var.enable_ephemeral_restore_latest ? 1 : 0
   project_id   = data.mongodbatlas_cloud_backup_snapshots.this[0].project_id
   cluster_name = data.mongodbatlas_cloud_backup_snapshots.this[0].cluster_name
-  snapshot_id  = jsondecode(data.mongodbatlas_cloud_backup_snapshots.this[0].id)
+  snapshot_id  = data.mongodbatlas_cloud_backup_snapshots.this[0].snapshot_ids
   delivery_type_config {
     automated           = true
     target_cluster_name = mongodbatlas_cluster.this.name
