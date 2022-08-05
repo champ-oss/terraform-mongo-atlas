2dfd5c7ef8f06fda95f272175396518b00cdd93a
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-05 15:29:01 -0500

    updating examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -10,12 +10,21 @@ module "this" {
   cluster_name = "cluster-name"
   component    = "component-team"
 
-  /* create snapshot from source to target
-  enable_ephemeral_restore = true
-  target_project_id     = "123456789"
-
-  cluster snapshot restore
+  /* update current cluster with certain snapshot id
   enable_cluster_restore = true
-  snapshot_id            = "123456789"
+  snapshot_id            = "12344"
+
+  update new env with certain snapshot id from another project
+  enable_ephemeral_restore = true
+  source_backup_project_id = "12345"
+  source_snapshot_id       = "1234"
+
+  update new env with latest snapshot id from project
+  enable_ephemeral_restore_latest = true
+  source_backup_latest_project_id = "12345"
+
+  create snapshot and restore on target project
+  enable_snapshot_target_restore = true
+  target_project_id              = "12345"
   */
 }
