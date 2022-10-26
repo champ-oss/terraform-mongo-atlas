21abeb9d0e0ca18edc4b04c097b686cfce9247ff
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-10-26 11:59:55 -0400

    lint

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,21 +9,22 @@ module "this" {
   org_id                      = var.org_id
   cluster_name                = "cluster-name"
   provider_instance_size_name = "M10"
-  replication_specs = [{
-    num_shards : 1,
-    zone_name : "ZONE 2 managed by terraform",
-    region_name : "US_EAST_2",
-    electable_nodes : 3,
-    priority : 7,
-    read_only_nodes : 0
-  }]
+  replication_specs           = [
+    {
+      num_shards : 1,
+      zone_name : "ZONE 2 managed by terraform",
+      region_name : "US_EAST_2",
+      electable_nodes : 3,
+      priority : 7,
+      read_only_nodes : 0
+    }
+  ]
 
   disk_size_gb                 = 10
   cloud_backup                 = true
   auto_scaling_disk_gb_enabled = false
   provider_name                = "AWS"
   mongo_region                 = "US_EAST_2"
-}
 
   /*
   replication specs not available with free tier, example below
