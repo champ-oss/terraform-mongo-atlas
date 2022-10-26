6f18f9760fb3366861be0feae92cac54ec3950bc
Phillip Langley <philliplangley@CHAMP-MacBook-Pro-Phillip-Langley.local>
2022-10-26 11:54:58 -0400

    setting dedicted and 6.x version

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,13 +3,27 @@ locals {
 }
 
 module "this" {
-  source                 = "../../"
-  project_name           = "shared-example"
-  git                    = local.git
-  org_id                 = var.org_id
-  cluster_name           = "cluster-name"
-  component              = "component-team"
-  mongo_db_major_version = "5.0"
+  source                      = "../../"
+  project_name                = "mongo-example"
+  git                         = local.git
+  org_id                      = var.org_id
+  cluster_name                = "cluster-name"
+  provider_instance_size_name = "M10"
+  replication_specs = [{
+    num_shards : 1,
+    zone_name : "ZONE 2 managed by terraform",
+    region_name : "US_EAST_2",
+    electable_nodes : 3,
+    priority : 7,
+    read_only_nodes : 0
+  }]
+
+  disk_size_gb                 = 10
+  cloud_backup                 = true
+  auto_scaling_disk_gb_enabled = false
+  provider_name                = "AWS"
+  mongo_region                 = "US_EAST_2"
+}
 
   /*
   replication specs not available with free tier, example below
