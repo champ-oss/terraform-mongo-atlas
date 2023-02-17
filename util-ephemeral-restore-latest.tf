1c0ee3c6493e9fa4b9b0a9657c9f73057bf2300d
Phillip Langley <philliplangley@champmbplangley.lan>
2023-02-17 17:23:34 -0600

    testing retry

--- util-ephemeral-restore-latest.tf
+++ util-ephemeral-restore-latest.tf
@@ -24,16 +24,23 @@ resource "mongodbatlas_cloud_backup_snapshot_restore_job" "mongodbatlas_cloud_ba
   depends_on = [mongodbatlas_cluster.this]
 }
 
+data "mongodbatlas_cloud_backup_snapshot_restore_job" "this" {
+  count = var.enable_ephemeral_restore_latest ? 1 : 0
+  project_id   = data.mongodbatlas_clusters.ephemeral_restore_latest[0].project_id
+  cluster_name = data.mongodbatlas_clusters.ephemeral_restore_latest[0].results[0].name
+  job_id       = mongodbatlas_cloud_backup_snapshot_restore_job.mongodbatlas_cloud_backup_snapshot_restore_job[0].snapshot_restore_job_id
+}
+
 resource "null_resource" "exec_mongo-restore-job-status-check" {
   count = var.enable_ephemeral_restore_latest ? 1 : 0
   provisioner "local-exec" {
 
-    command     = "sleep 45 && chmod +x ${path.module}/mongo-restore-job-api.py;pip3 install retry;python ${path.module}/mongo-restore-job-api.py"
+    command     = "sleep 30 && chmod +x ${path.module}/mongo-restore-job-api.py;pip3 install retry;python ${path.module}/mongo-restore-job-api.py"
     interpreter = ["bash", "-c"]
     environment = {
-      MONGODB_ATLAS_GROUP_ID       = mongodbatlas_cloud_backup_snapshot_restore_job.mongodbatlas_cloud_backup_snapshot_restore_job[0].project_id
-      MONGODB_ATLAS_CLUSTER_NAME   = mongodbatlas_cloud_backup_snapshot_restore_job.mongodbatlas_cloud_backup_snapshot_restore_job[0].cluster_name
-      MONGODB_ATLAS_RESTORE_JOB_ID = mongodbatlas_cloud_backup_snapshot_restore_job.mongodbatlas_cloud_backup_snapshot_restore_job[0].snapshot_restore_job_id
+      MONGODB_ATLAS_GROUP_ID       = data.mongodbatlas_cloud_backup_snapshot_restore_job.this[0].project_id
+      MONGODB_ATLAS_CLUSTER_NAME   = data.mongodbatlas_cloud_backup_snapshot_restore_job.this[0].cluster_name
+      MONGODB_ATLAS_RESTORE_JOB_ID = data.mongodbatlas_cloud_backup_snapshot_restore_job.this[0].job_id
     }
   }
   depends_on = [mongodbatlas_cloud_backup_snapshot_restore_job.mongodbatlas_cloud_backup_snapshot_restore_job]
