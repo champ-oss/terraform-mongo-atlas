e783c3c62c7721fa1def4aec1a65edc5429b3eea
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-05-26 19:05:15 -0500

    testing updated changes

--- cluster.tf
+++ cluster.tf
@@ -1,6 +1,6 @@
 resource "mongodbatlas_cluster" "this" {
   project_id = mongodbatlas_project.this.id
-  name       = trimsuffix(substr("${var.cluster_name}-${random_string.limit.result}", 0, 23), "-") # trim hyphen if last character
+  name       = trimsuffix(substr("${var.cluster_name}-${random_string.limit.result}", 0, 23), "-")
 
   # Provider Settings "block"
   # setting backing provider name to null as it's not available for provider_instance_size_name >= M10
