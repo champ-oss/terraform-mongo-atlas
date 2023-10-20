a6095bab85d4bb2e7f01815bb1161ece5b652741
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-20 10:52:52 -0500

    trim mongo prefix from ssm

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "mongo_read_only" {
-  name  = "/${var.git}/mongo/${mongodbatlas_cluster.this.connection_strings[0].standard_srv}/password"
+  name  = trim("/${var.git}/mongo/${mongodbatlas_cluster.this.connection_strings[0].standard_srv}/password", "mongodb+srv://")
   type  = "SecureString"
   value = random_password.read_only.result
   tags = {
