e06741cc90059ba1fa3faa41f51f7c18bc253870
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-20 12:16:27 -0500

    updating ssm name requirements

--- ssm.tf
+++ ssm.tf
@@ -1,10 +1,11 @@
 resource "aws_ssm_parameter" "mongo_read_only" {
-  name  = split("//", "/${var.git}/mongodbatlas_cluster.this.connection_strings[0].standard_srv)[1]")
+  name  = var.project_name
   type  = "SecureString"
   value = random_password.read_only.result
   tags = {
-    cost    = "shared"
-    creator = "terraform"
-    git     = var.git
+    cost     = "shared"
+    creator  = "terraform"
+    git      = var.git
+    endpoint = split("//", mongodbatlas_cluster.this.connection_strings[0].standard_srv)[1]
   }
 }
