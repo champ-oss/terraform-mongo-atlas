eddd11ea26ec6061a30527a980e20c916e7fb8e9
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-11 15:01:31 -0500

    testing with module disabled

--- project-ip-access-list.tf
+++ project-ip-access-list.tf
@@ -1,5 +1,5 @@
 resource "mongodbatlas_project_ip_access_list" "this" {
-  for_each   = var.enabled ? toset(var.project_ip_address_allow_list) : {}
+  for_each   = var.enabled ? var.project_ip_address_allow_list : []
   project_id = mongodbatlas_project.this[0].id
   cidr_block = each.value
   comment    = "adding network ip address to allow connection"
