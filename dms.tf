3548011d4de29ce64f8b7894726295df3b385bd6
jeremiah-norris-champ <77309283+jeremiah-norris-champ@users.noreply.github.com>
2025-02-27 10:17:39 -0600

    update shard calculation for disabled

--- dms.tf
+++ dms.tf
@@ -1,7 +1,7 @@
 locals {
   # example: mongodb://my-test-shard-00-00.nvn238v.mongodb.net:27017,my-test-shard-00-01.nvn238v.mongodb.net:27017,my-test-shard-00-02.nvn238v.mongodb.net:27017
   shards_map = var.create_dms_endpoint ? [
-    for shard in split(",", trimprefix(mongodbatlas_cluster.this[0].mongo_uri, "mongodb://")) : {
+    for shard in split(",", trimprefix(try(mongodbatlas_cluster.this[0].mongo_uri, ""), "mongodb://")) : {
       host = split(":", shard)[0]
       port = split(":", shard)[1]
     }
