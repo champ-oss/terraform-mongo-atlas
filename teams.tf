406708711f8003edd6b1cfaeb4c3f91d71e6e604
Chad Hale <chadhale@Mac.lan>
2025-06-13 15:59:17 -0500

    enabled flag for teams CLOUD-3124

--- teams.tf
+++ teams.tf
@@ -1,6 +1,5 @@
 resource "mongodbatlas_teams" "this" {
-  count    = var.enabled && length(var.teams) > 0 ? 1 : 0
-  for_each = var.teams
+  for_each = var.enabled && length(var.teams) > 0 ? var.teams : {}
 
   org_id    = var.org_id
   name      = "${var.component}-${random_string.this[0].result}-${each.value.name_suffix}"
